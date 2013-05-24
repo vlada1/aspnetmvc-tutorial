b31b5fb6855a2e57cdda2b419769c2369fa5a71c
bnirav23 <bnirav23@gmail.com>
2013-05-24 00:53:38 +0000

    Passing model to view + Intro to unit testing

---
+++ PhotoShare/PhotoShare/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using PhotoShare.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoShare.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var model = new AboutModel();
+            model.Name = "Nirav";
+            model.Location = "Atlanta,GA";
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
