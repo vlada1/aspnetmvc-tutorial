49722f8edaf4da3d12aed13c65a08cc509f878fe
bnirav23 <bnirav23@gmail.com>
2013-06-27 16:16:57 +0000

    Data - Setting up database as per Code First Approach + Setting up, creating, and running Migrations

---
+++ PhotoShare/PhotoShare/Models/PhotoShareDb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PhotoShare.Models
+{
+    public class PhotoShareDb : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+    }
+}
