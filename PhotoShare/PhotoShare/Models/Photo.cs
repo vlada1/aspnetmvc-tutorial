1a29b982491bac00cca1b066638de85fdeba3fc5
bnirav23 <bnirav23@gmail.com>
2013-07-04 23:16:06 +0000

    Data - File Uploading + Preventing Mass Assignment + Validations

--- PhotoShare/PhotoShare/Models/Photo.cs
+++ PhotoShare/PhotoShare/Models/Photo.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace PhotoShare.Models
 {
     public class Photo
     {
         public int ID { get; set; }
+
+        [Required]
+        [StringLength(140)]
+        [Display(Name="Blurb")]
         public string Description { get; set; }
+
         public string FilePath { get; set; }
+        
+        [Range(1,10)]
         public int Rating { get; set; }
         public int AlbumID { get; set; }
     }
