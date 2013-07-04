3d51502caa6e13b2c3c36aead6cb247555e0c27f
bnirav23@gmail.com <bnirav23@gmail.com@a0699beb-b307-eeb7-6f27-1c298032753e>
2013-07-04 23:16:06 +0000

    Data - File Uploading + Preventing Mass Assignment + Validations

--- trunk/PhotoShare/PhotoShare/Controllers/PhotosController.cs
+++ trunk/PhotoShare/PhotoShare/Controllers/PhotosController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
+using System.IO;
+using System.Data;
 
 namespace PhotoShare.Controllers
 {
@@ -19,6 +21,59 @@ namespace PhotoShare.Controllers
         {
             var model = db.Albums.Find(id);
             return View(model);
+        }
+
+        public ActionResult Create(int albumId)
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Photo photo, HttpPostedFileBase photoFile)
+        {
+            if (ModelState.IsValid)
+            {
+                var fileName = Path.GetFileName(photoFile.FileName);
+                var path = Path.Combine(Server.MapPath("~/PhotoUploads"), fileName);
+                photoFile.SaveAs(path);
+                photo.FilePath = "~/PhotoUploads/" + photoFile.FileName;
+                db.Photos.Add(photo);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Photos", new { id = photo.AlbumID });
+            }
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var model = db.Photos.Find(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Photo photo, HttpPostedFileBase photoFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (photoFile != null)
+                {
+                    var fileName = Path.GetFileName(photoFile.FileName);
+                    var path = Path.Combine(Server.MapPath("~/PhotoUploads"), fileName);
+                    photoFile.SaveAs(path);
+                    photo.FilePath = "~/PhotoUploads/" + photoFile.FileName;
+                }
+                db.Entry(photo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index", "Photos", new { id = photo.AlbumID });
+            }
+            return View(photo);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
 
     }
