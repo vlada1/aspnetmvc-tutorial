811abd24c2d817c02175544cf7835c66090817e5
bnirav23@gmail.com <bnirav23@gmail.com@a0699beb-b307-eeb7-6f27-1c298032753e>
2013-07-12 00:25:12 +0000

    AJAX - Paging

--- trunk/PhotoShare/PhotoShare/Scripts/photoshare.js
+++ trunk/PhotoShare/PhotoShare/Scripts/photoshare.js
@@ -1,6 +1,22 @@
 ﻿
 $(function () {
 
+    function ajaxFormSubmit() {
+        var $form = $(this);
+        var options = {
+            url: $form.attr("action"),
+            type: $form.attr("method"),
+            data: $form.serialize()
+        };
+        $.ajax(options).done(function (data) {
+            var $target = $($form.attr("data-photoshare-target"));
+            var $newHtml = $(data);
+            $target.replaceWith($newHtml);
+            $newHtml.effect("highlight");
+        });
+        return false;
+    }
+
     function submitAutoComplete(event, ui) {
         var $input = $(this);
         $input.val(ui.item.label);
@@ -19,6 +35,22 @@ $(function () {
         $input.autocomplete(options);
     }
 
+    function getPage() {
+        var $a = $(this);
+        var options = {
+            url: $a.attr("href"),
+            data:$("form").serialize(),
+            type:"get"
+        };
+        $.ajax(options).done(function (data) {
+            var target = $a.parents("div.pagedList").attr("data-photoshare-target");
+            $(target).replaceWith(data);
+        });
+        return false;
+    }
+
     $("input[data-photoshare-autocomplete]").each(createAutoComplete);
+    $("form[data-photoshare-ajax='true']").submit(ajaxFormSubmit);
+    $(".main-content").on("click", ".pagedList a", getPage);
 
 });
